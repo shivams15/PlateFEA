f75fd5dcb6447ba4033e60f14f697507f9269afa
Shivam <shivam@buffalo.edu>
2018-11-19 21:43:43 -0500

    Cleaning up the code

--- SRC/FEMSolver.h
+++ SRC/FEMSolver.h
@@ -5,14 +5,14 @@
 
 class FEMSolver{
 private:
-	Vec u, sxx, syy, sxy;		//Solution vectors for displacements and stresses
-	Vec f, fxx, fyy, fxy;		//Nodal force vectors
-	Mat K, L;					//Stiffness matrices 
-	KSP uSolver;				//KSP solver for displacements
-	KSP sSolver;				//KSP solver for stresses
+	Vec u, sxx, syy, sxy;	//Solution vectors for displacements and stresses
+	Vec f, fxx, fyy, fxy;	//Nodal force vectors
+	Mat K, L;				//Stiffness matrices 
+	KSP uSolver;			//KSP solver for displacements
+	KSP sSolver;			//KSP solver for stresses
 	Grid *grid;
-	double nu;					//Poisson's ratio
-	double E;					//Young's modulus
+	double nu;				//Poisson's ratio
+	double E;				//Young's modulus
 	void SolverSetup();
 	void ExportData();
 	void AssembleStiffnessMatrix();
